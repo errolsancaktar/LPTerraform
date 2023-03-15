ed8a2eb32463c105f746e27063c0180856b8fde1
Errol Sancaktar <errol@sancaktar.net>
2023-03-15 16:49:38 -0600

    removed output

--- output.tf
+++ output.tf
@@ -2,11 +2,6 @@ output "load_balancer_ip" {
   value = module.lb-http.external_ip
 }
 
-output "db_pass" {
-  value = random_password.db.result
-  sensitive = true
-}
-
 output "db_ip" {
   value = module.pg_db.private_ip_address
 }
